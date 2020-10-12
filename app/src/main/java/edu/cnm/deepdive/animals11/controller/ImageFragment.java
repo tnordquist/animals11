e2e1a14068fcc6b3ff382a0c46bdbd439e55560c
Todd A Nordquist <toddnordquist144@gmail.com>
2020-10-12 15:14:36 -0600

    Added custom spinner item.

--- app/src/main/java/edu/cnm/deepdive/animals11/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals11/controller/ImageFragment.java
@@ -62,7 +62,7 @@ public class ImageFragment extends Fragment implements OnItemSelectedListener {
       public void onChanged(List<Animal> animals) {
         ImageFragment.this.animals = animals;
         ArrayAdapter<Animal> adapter = new ArrayAdapter<>(
-            ImageFragment.this.getContext(), android.R.layout.simple_spinner_item, animals);
+            ImageFragment.this.getContext(), R.layout.custom_spinner_item, animals);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
