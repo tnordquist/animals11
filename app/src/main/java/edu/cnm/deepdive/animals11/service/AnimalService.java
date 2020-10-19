1990f766e017752fdaf5227bb7265355f8b831dd
Todd A Nordquist <toddnordquist144@gmail.com>
2020-10-19 10:10:04 -0600

    Replaced asynctask w/ rx

--- app/src/main/java/edu/cnm/deepdive/animals11/service/AnimalService.java
+++ app/src/main/java/edu/cnm/deepdive/animals11/service/AnimalService.java
@@ -1,9 +1,20 @@
 package edu.cnm.deepdive.animals11.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.animals11.BuildConfig;
 import edu.cnm.deepdive.animals11.model.Animal;
 import edu.cnm.deepdive.animals11.model.ApiKey;
+import io.reactivex.Single;
 import java.util.List;
+import java.util.Observable;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -11,11 +22,39 @@ import retrofit2.http.POST;
 
 public interface AnimalService {
 
+  static AnimalService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
   @GET("getKey")
-  Call<ApiKey> getApiKey();
+  Single<ApiKey> getApiKey();
 
   @FormUrlEncoded
   @POST("getAnimals")
-  Call<List<Animal>> getAnimals(@Field("key") String key);
+  Single<List<Animal>> getAnimals(@Field("key") String key);
+
+  class InstanceHolder {
+
+    private static final AnimalService INSTANCE;
+
+    static {
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl(BuildConfig.BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .client(client)
+          .build();
+      INSTANCE = retrofit.create(AnimalService.class);
+
+    }
+  }
 
 }
--- app/src/main/java/edu/cnm/deepdive/animals11/viewmodel/AnimalViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals11/viewmodel/AnimalViewModel.java
@@ -14,6 +14,7 @@ import edu.cnm.deepdive.animals11.BuildConfig;
 import edu.cnm.deepdive.animals11.model.Animal;
 import edu.cnm.deepdive.animals11.model.ApiKey;
 import edu.cnm.deepdive.animals11.service.AnimalService;
+import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.util.List;
 import retrofit2.Response;
@@ -22,12 +23,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AnimalViewModel extends AndroidViewModel {
 
-  private MutableLiveData<List<Animal>> animals;
+  private final MutableLiveData<List<Animal>> animals;
+  private final MutableLiveData<Throwable> throwable;
+  private final AnimalService animalService;
 
   public AnimalViewModel(
       @NonNull Application application) {
     super(application);
     animals = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    animalService = AnimalService.getInstance();
     loadAnimals();
   }
 
@@ -35,47 +40,19 @@ public class AnimalViewModel extends AndroidViewModel {
     return animals;
   }
 
-  @SuppressLint("StaticFieldLeak")
+  public MutableLiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  @SuppressLint("CheckResult")
   private void loadAnimals() {
 
-    new AsyncTask<Void, Void, List<Animal>>() {
-
-      AnimalService animalService;
-
-      @Override
-      protected void onPreExecute() {
-        super.onPreExecute();
-        Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .create();
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-        animalService = retrofit.create(AnimalService.class);
-      }
-
-      @Override
-      protected List<Animal> doInBackground(Void... voids) {
-
-        try {
-
-          Response<ApiKey> apiKeyResponse = animalService.getApiKey().execute();
-          ApiKey apiKey = apiKeyResponse.body();
-          final String key = apiKey.getKey();
-
-          Response<List<Animal>> animalResponse = animalService.getAnimals(key).execute();
-          List<Animal> animals = animalResponse.body();
-          AnimalViewModel.this.animals.postValue(animals);
-          return animals;
-
-        } catch (IOException e) {
-          Log.e("AnimalService", e.getMessage(), e);
-          cancel(true);
-        }
-
-        return null;
-      }
-    }.execute();
+    animalService.getApiKey()
+        .subscribeOn(Schedulers.io())
+        .flatMap((key) -> animalService.getAnimals(key.getKey()))
+        .subscribe(
+            animals::postValue,
+            throwable::postValue
+        );
   }
 }
